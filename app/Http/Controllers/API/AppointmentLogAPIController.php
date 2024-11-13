06b237b8138e75edfc20e4d9349be986338298e0
Cesar Chab <cesarchabuluac@gmail.com>
2024-11-13 16:37:17 -0600

    Fix request

--- app/Http/Controllers/API/AppointmentLogAPIController.php
+++ app/Http/Controllers/API/AppointmentLogAPIController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AppointmentLogController extends Controller
+class AppointmentLogAPIController extends Controller
 {
     /**
      * Store a newly created resource in storage.
