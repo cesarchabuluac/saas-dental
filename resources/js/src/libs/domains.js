07f9169bebb2ab466a60b677ab77a97e9a779577
Cesar Chab <cesarchabuluac@gmail.com>
2024-11-13 10:58:08 -0600

    Add compilled assets

--- resources/js/src/libs/domains.js
+++ resources/js/src/libs/domains.js
@@ -1,9 +1,25 @@
+// export function checkIsCentral() {
+//     const host = window.location.host;
+//     console.log(`Host: ${host}`);
+//     const isTenant = host.split('.').length > 1;
+//     console.log(`Is tenant: ${isTenant}`);
+
+//     return !isTenant;
+// }
+
 export function checkIsCentral() {
     const host = window.location.host;
     console.log(`Host: ${host}`);
-    const isTenant = host.split('.').length > 1;
-    console.log(`Is tenant: ${isTenant}`);
 
-    return !isTenant;
+    // Definir los dominios principales de la aplicación central
+    const centralDomains = ['fichadentales.com', 'www.fichadentales.com', 'localhost:8000', 'localhost'];
+
+    // Verificar si el dominio actual es uno de los dominios centrales
+    const isCentral = centralDomains.includes(host);
+
+    console.log(`Is central: ${isCentral}`);
+
+    return isCentral;  // Retorna true si es central, false si es inquilino
 }
+
   
